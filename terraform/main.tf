1ece83690f14569856c5850365787872c26df832
saudkhalid302 <saudkhalid302@gmail.com>
2026-03-17 00:27:15 +0100

    Fixed resource names

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,36 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Bucket jise tum use kar rahe ho
 resource "aws_s3_bucket" "currency_bucket" {
-  bucket = "capstone-currency-data-saud-2026"
+  bucket = "capstone-currency-data-saud-final-2026"
 }
 
-# Output taake tumhein pata ho bucket ka naam kya hai
-output "s3_bucket_name" {
-  value = aws_s3_bucket.currency_bucket.id
-}
-
-# Yahan par apna naya IAM aur permissions wala code add karo
 resource "aws_iam_user" "data_engineer" {
-  name = "data-engineer-saud"
+  name = "data-engineer-saud-final"
 }
 
 resource "aws_iam_user_policy" "s3_access" {
-  name = "s3-access-policy"
+  name = "s3-access-policy-final"
   user = aws_iam_user.data_engineer.name
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"]
-        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
         Resource = [
-          "arn:aws:s3:::capstone-currency-data-saud-2026",
-          "arn:aws:s3:::capstone-currency-data-saud-2026/*"
+          "arn:aws:s3:::capstone-currency-data-saud-final-2026",
+          "arn:aws:s3:::capstone-currency-data-saud-final-2026/*"
         ]
       }
     ]
   })
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.currency_bucket.bucket
 }
